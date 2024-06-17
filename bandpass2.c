d3aa41c06566a8822d2db9a97c14566c23b9139c
Koyo Uekubo <uekubo-koyo343@g.ecc.u-tokyo.ac.jp>
2024-06-17 14:45:03 +0900

    connect bandpass2.c and phone.c

---
+++ bandpass2.c
@@ -0,0 +1,145 @@
+/* 
+ * bandpass.c
+ * 使い方
+ *   ./fft n a b
+ * 
+ * 以下を繰り返す:
+ *   標準入力から, 16 bit integerをn個読む
+ *   FFTする
+ *   周波数fが a<f<b を満たす周波数成分だけ残す
+ *   逆FFTする
+ *   標準出力へ出す
+ *
+ * したがってバンドパスフィルタになる
+ * 
+ */
+#include <assert.h>
+#include <complex.h>
+#include <math.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+typedef unsigned char sample_t;
+
+void die(char * s) {
+  perror(s); 
+  exit(1);
+}
+
+/* 標本(整数)を複素数へ変換 */
+void sample_to_complex(sample_t * s, 
+		       complex double * X, 
+		       long n) {
+  long i;
+  for (i = 0; i < n; i++) X[i] = s[i];
+}
+
+/* 複素数を標本(整数)へ変換. 虚数部分は無視 */
+void complex_to_sample(complex double * X, 
+		       sample_t * s, 
+		       long n) {
+  long i;
+  for (i = 0; i < n; i++) {
+    s[i] = creal(X[i]);
+  }
+}
+
+/* 高速(逆)フーリエ変換;
+   w は1のn乗根.
+   フーリエ変換の場合   偏角 -2 pi / n
+   逆フーリエ変換の場合 偏角  2 pi / n
+   xが入力でyが出力.
+   xも破壊される
+ */
+void fft_r(complex double * x, 
+	   complex double * y, 
+	   long n, 
+	   complex double w) {
+  if (n == 1) { y[0] = x[0]; }
+  else {
+    complex double W = 1.0; 
+    long i;
+    for (i = 0; i < n/2; i++) {
+      y[i]     =     (x[i] + x[i+n/2]); /* 偶数行 */
+      y[i+n/2] = W * (x[i] - x[i+n/2]); /* 奇数行 */
+      W *= w;
+    }
+    fft_r(y,     x,     n/2, w * w);
+    fft_r(y+n/2, x+n/2, n/2, w * w);
+    for (i = 0; i < n/2; i++) {
+      y[2*i]   = x[i];
+      y[2*i+1] = x[i+n/2];
+    }
+  }
+}
+
+void fft(complex double * x, 
+	 complex double * y, 
+	 long n) {
+  long i;
+  double arg = 2.0 * M_PI / n;
+  complex double w = cos(arg) - 1.0j * sin(arg);
+  fft_r(x, y, n, w);
+  for (i = 0; i < n; i++) y[i] /= n;
+}
+
+void ifft(complex double * y, 
+	  complex double * x, 
+	  long n) {
+  double arg = 2.0 * M_PI / n;
+  complex double w = cos(arg) + 1.0j * sin(arg);
+  fft_r(y, x, n, w);
+}
+
+int pow2check(long N) {
+  long n = N;
+  while (n > 1) {
+    if (n % 2) return 0;
+    n = n / 2;
+  }
+  return 1;
+}
+
+int bandpass(long n, unsigned char *buf) {
+  if (!pow2check(n)) {
+    fprintf(stderr, "error : n (%ld) not a power of two\n", n);
+    exit(1);
+  }
+
+  complex double * X = calloc(sizeof(complex double), n);
+  complex double * Y = calloc(sizeof(complex double), n);
+
+  while (1) {
+    /* 複素数の配列に変換 */
+    sample_to_complex(buf, X, n);
+    /* FFT -> Y */
+    fft(X, Y, n);
+    
+    // 周波数の重心を計算
+    double integral1 = 0;
+    double integral2 = 0;
+    for (int l = 1; l < n; ++l) {
+      integral1 += l * cabs(Y[l]) / n/2;
+      integral2 += cabs(Y[l]) / n/2;
+    }
+
+    double g_l = integral1/integral2; // 重心
+    double a = 0.1*g_l; // 下限
+    double b = 1.5*g_l; // 上限
+    
+    // 重心の上下をカット＋音声が小さくなるので少し大きくする
+    for (int l = 0; l < n; ++l) {
+      if (l >= a && l <= b) Y[l] *= 1.2;
+      else Y[l] = 0;
+    }
+
+    /* IFFT -> Z */
+    ifft(Y, X, n);
+    /* 標本の配列に変換 */
+    complex_to_sample(X, buf, n);
+  }
+
+  return 0;
+}
--- phone.c
+++ phone.c
@@ -9,11 +9,13 @@
 #include <string.h>
 #include <pthread.h>
 
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 32768
+
+int bandpass(long n, unsigned char *buffer);
 
 void *send_audio(void *arg) {
   int sock = *((int *)arg);
-  unsigned char buffer[BUFFER_SIZE];
+  unsigned char* buffer = malloc(sizeof(unsigned char) * BUFFER_SIZE);
   while (1) {
       ssize_t num_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
       if (num_read <= 0) break;
@@ -22,13 +24,36 @@ void *send_audio(void *arg) {
   return NULL;
 }
 
+void *send_audio2(void *arg) {
+  int sock = *((int *)arg);
+  unsigned char* buffer = malloc(sizeof(unsigned char) * BUFFER_SIZE);
+  while (1) {
+      ssize_t num_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+      if (num_read <= 0) break;
+      bandpass(BUFFER_SIZE, buffer);
+      send(sock, buffer, num_read, 0);
+  }
+  return NULL;
+}
+
 void *receive_audio(void *arg) {
   int sock = *((int *)arg);
-  unsigned char buffer[BUFFER_SIZE];
+  unsigned char* buffer = malloc(sizeof(unsigned char) * BUFFER_SIZE);
   while (1) {
     ssize_t num_received = recv(sock, buffer, BUFFER_SIZE, 0);
     if (num_received <= 0) break;
+    write(STDOUT_FILENO, buffer, num_received);
+  }
+  return NULL;
+}
 
+void *receive_audio2(void *arg) {
+  int sock = *((int *)arg);
+  unsigned char* buffer = malloc(sizeof(unsigned char) * BUFFER_SIZE);
+  while (1) {
+    ssize_t num_received = recv(sock, buffer, BUFFER_SIZE, 0);
+    if (num_received <= 0) break;
+    bandpass(BUFFER_SIZE, buffer);
     write(STDOUT_FILENO, buffer, num_received);
   }
   return NULL;
@@ -69,6 +94,8 @@ int server_setup(int port){
   return s;
 }
 
+
+
 int main(int argc, char *argv[]) {
   if ((argc < 2) || (argc > 3)) {
       fprintf(stderr, "Usage Error\n");
@@ -87,18 +114,42 @@ int main(int argc, char *argv[]) {
     sock = server_setup(port);
   }
 
-  pthread_t send_thread, receive_thread;
-
-  if (pthread_create(&send_thread, NULL, send_audio, &sock) != 0) {
-      perror("pthread_create send_audio");
-      close(sock);
+  FILE *fp1 = NULL;
+  char *cmdline = "rec -t raw -b 16 -c 1 -e s -r 44100 - & play -t raw -b 16 -c 1 -e s -r 44100 -";
+  if ((fp1 = popen(cmdline, "r")) == NULL) {
+      perror("popen");
       exit(EXIT_FAILURE);
   }
 
-  if (pthread_create(&receive_thread, NULL, receive_audio, &sock) != 0) {
-      perror("pthread_create receive_audio");
-      close(sock);
-      exit(EXIT_FAILURE);
+  pthread_t send_thread, receive_thread;
+
+  char use_bandpass = 1;  //  フィルタをかける場合1、かけない場合0
+
+  if (use_bandpass) {
+    if (pthread_create(&send_thread, NULL, send_audio2, &sock) != 0) {
+        perror("pthread_create send_audio");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_create(&receive_thread, NULL, receive_audio2, &sock) != 0) {
+        perror("pthread_create receive_audio");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+  }
+  else {
+    if (pthread_create(&send_thread, NULL, send_audio, &sock) != 0) {
+        perror("pthread_create send_audio");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pthread_create(&receive_thread, NULL, receive_audio, &sock) != 0) {
+        perror("pthread_create receive_audio");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
   }
 
   pthread_join(send_thread, NULL);
